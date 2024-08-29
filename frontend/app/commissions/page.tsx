43450dcc158643285089b611af217922435c1f2e
rennahintermeister <renna.hintermeister@lab651.com>
2024-08-29 14:55:18 -0500

    Add Footer and Header

--- frontend/app/commissions/page.tsx
+++ frontend/app/commissions/page.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function Commissions() {
     return (
       <main className="flex min-h-screen flex-col items-center p-24">
             <div className="title text-5xl">
---
+++ frontend/components/Header.tsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        Renna's Gallery and Commissions
+      </main>
+    );
+  }
